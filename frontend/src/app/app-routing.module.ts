865e6f494299ad306ce1e4f2ffad8349d94bf2f0
liam-seddon <liamseddon@talktalk.net>
2020-05-11 21:43:51 +0100

    added the page import

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {taskmanagerviewcomponent} from './pages/task-manager-view/task-manager-view.component';
 
 
 const routes: Routes = [
   {path: '', component: taskmanagerviewcomponent}
-  
+
 
 ];
 
