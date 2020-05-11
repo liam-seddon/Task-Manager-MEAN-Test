3a42c469808f92ad3b0fe8be178b1b034c321186
liam-seddon <liamseddon@talktalk.net>
2020-05-12 00:14:58 +0100

    created new folder for database files and added mongo.js for mongodb conenction

--- backend/api/app.js
+++ backend/api/app.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app =express();
 
-app.get('/', (req, res) => {
-  res.send('Hello!');
+//to get lists from database
+app.get('/lists', (req, res) => {
+
+})
+
+//to create lists to database
+app.get('/lists', (req, res) => {
+
+})
+
+//to update lists in the database
+app.patch('/lists/:id', (req, res) => {
+
+})
+//to delete lists in the database
+app.delete('/lists/:id', (req, res) => {
+
 })
 
 
 app.listen(3000, () =>{
-  console.log ("server is connected and listening!");
-  })
+  console.log("server is connected and listening!");
+})
