28b58a833ed3a982751714be5bd7ab975aa816b8
liam-seddon <liamseddon@talktalk.net>
2020-05-12 02:23:35 +0100

    package updated, new code added to app.js

--- backend/api/app.js
+++ backend/api/app.js
@@ -1,19 +1,36 @@
 const express = require('express');
 const app =express();
+const bodyParser = require('body-parser');
+const { mongoose } = require('./database/mongo');
 
 //Mongoose Models
-const {List} = require('.databse/models/list.model');
+const { List, Task } = require('./database/models');
+
+//Middleware
+app.use(bodyParser.json());
 
 
 //to get lists from database
 app.get('/lists', (req, res) => {
+  List.find({}).then(lists) =>{
+    res.send(lists;)
+
+  };
 
 })
 
 //to create lists to database
-app.get('/lists', (req, res) => {
+app.post('/lists', (req, res) => {
+  let title = req.body.title;
 
-})
+  let newList = new List({
+    title
+  });
+  newList.save().then(listDoc) => {
+    res.send(listDoc)
+  })
+
+});
 
 //to update lists in the database
 app.patch('/lists/:id', (req, res) => {
